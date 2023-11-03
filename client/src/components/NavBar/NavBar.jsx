dedcb7134ca07425fe9707c6bd9c0ddaefb1ada7
Michelle Diaz <120993737+Michellemishna@users.noreply.github.com>
2023-11-03 17:39:35 -0600

    Update NavBar.jsx

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -18,7 +18,7 @@ const NavBar = () => {
         </a>
 
         <a target="_blank" rel="noopener noreferrer" href='https://es.reactjs.org/'>
-          <img alt="img" src="http://ibthemespro.com/docs/beny/img/side-nav/cmm4.png"/>
+          <img alt="img" src="https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/2EP14mWkbx9sq03nWnRSGT/f1d22d88bb5dde030275f9520c0f2e92/React_YT_Thumbnail.png"/>
         </a>
             
         <Link to='/home'>
